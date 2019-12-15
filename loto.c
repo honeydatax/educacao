0938d1122aa7094bd7ab34fc397652c01c7eb4ea
lisbon <16432726+honeydatax@users.noreply.github.com>
2019-12-15 12:06:26 +0000

    loto numbers

---
+++ loto.c
@@ -0,0 +1,15 @@
+#include "pt.h"
+
+
+inteiro comecar(){
+	numero n=0;
+	inteiro i;
+    aleatorio(horas(vazio));
+	apagar();
+
+	localiza(0,1);
+	entre(i=0;i<10;i++){
+        escrevern(loto(49)+1);
+    }
+
+}
