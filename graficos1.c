bd953bfb06f624b0801ed33c012b922f877aea70
lisbon <16432726+honeydatax@users.noreply.github.com>
2019-12-16 14:28:42 +0000

    programar em portugues

--- graficos1.c
+++ graficos1.c
@@ -1,5 +1,7 @@
 #include "graficos.h"
 
+numero x=0.00;
+numero y=0.00;
 funcao desenhar(mecanismo *giringonca,desenho *desenho1,pontos apontador1);
 funcao relogio(pontos apontador1);
 funcao tecla(mecanismo geringoca,eventos evento1, pontos apontador1);
@@ -8,6 +10,7 @@ rotina saida();
 inteiro comecar(){
     texto titulo1[]="ola mundo";
     titulos=titulo1;
+    orelogio=relogio;
     desenhando=desenhar;
     saindo=saida;
     iniciar();
@@ -21,11 +24,23 @@ funcao desenhar(mecanismo *giringonca,desenho *desenho1,pontos apontador1){
 
     lapis(desenho1,0.0,0.0,0.0);
 
-    rectangulo(desenho1,10.0,10.0,100.00,100.00);
+    rectangulo(desenho1,x,y,100.00,100.00);
     preservar(desenho1);
     preechimento(desenho1);
 
 
     retornar falso;
 
+}
+
+
+
+funcao relogio(pontos apontador1){
+    x=x+1.00;
+    y=y+1.00;
+    if (x>300 || y>300){
+        x=0.00;
+        y=x;
+    }
+    redesenhar(esquema,0.00,0.00,largura,altura);
 }
