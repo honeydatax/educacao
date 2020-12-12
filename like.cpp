da3fd678a3eb8b98fb1f7c5e5305c876e0f41856
lisbon <16432726+honeydatax@users.noreply.github.com>
2020-12-12 18:59:13 +0000

    = copy oparetor

---
+++ like.cpp
@@ -0,0 +1,84 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+class sstring{
+public :
+char *text=NULL;
+sstring(){
+}
+void report(){
+	printf("%s\n",text);
+}
+sstring& operator +=(sstring& s){
+	char *sss;
+	char *ss;
+	size_t size=strlen(text)+4+strlen(s.text)+4;
+	sss=text;
+	ss=text+strlen(text)+2;
+	if(text==NULL || ss[0]!='!'){
+		text=(char*)(malloc(size));
+		strcpy(text,sss);
+	}else{
+		text=(char*)(realloc(text,size));		
+	}
+	strcat(text,s.text);
+	ss=text+strlen(text)+2;
+	ss[0]='!';
+	return *this;
+}
+
+sstring& operator +(sstring& s){
+	char *sss;
+	char *ss;
+	size_t size=strlen(text)+4+strlen(s.text)+4;
+	sss=text;
+	ss=text+strlen(text)+2;
+	if(text==NULL || ss[0]!='!'){
+		text=(char*)(malloc(size));
+		strcpy(text,sss);
+	}else{
+		text=(char*)(realloc(text,size));		
+	}
+	strcat(text,s.text);
+	ss=text+strlen(text)+2;
+	ss[0]='!';
+	return *this;
+}
+
+sstring& operator =(sstring& s){
+	char *sss;
+	char *ss;
+	size_t size=strlen(s.text)+4;
+	sss=text;
+	ss=text+strlen(text)+2;
+	if(text==NULL || ss[0]!='!'){
+		text=(char*)(malloc(size));
+	}else{
+		text=(char*)(realloc(text,size));		
+	}
+	strcpy(text,s.text);
+	ss=text+strlen(text)+2;
+	ss[0]='!';
+	return *this;
+}
+
+
+};
+
+
+
+int main(){
+	sstring s;
+	sstring ss;
+	sstring sss;
+	s.text=(char*)("hello world");
+	ss.text=(char*)(" hi ");
+	sss.text=(char*)(" there ");
+	s.report();
+	s=ss;
+	s.report();
+	s=s+sss;
+	s.report();
+	return 0;
+}
